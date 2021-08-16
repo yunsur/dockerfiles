8a033881c4e6d8d646ef0cf067f41b657c6e1707
yunsur <yunsur.shi@gmail.com>
2021-08-16 09:26:42 +0800

    Update alpine-postgres

--- alpine-postgres/install_extension.sql
+++ alpine-postgres/install_extension.sql
@@ -1,6 +1,9 @@
+-- Install pg_trgm
+CREATE EXTENSION pg_trgm;
+
 -- Install zhparser
 CREATE EXTENSION zhparser;
-CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
+CREATE TEXT SEARCH CONFIGURATION pg_catalog.chinese (PARSER = zhparser);
 ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple;
 
 --- Install pg_stat_statements
